42430c630b4f8fbcf575baaf8c2660aad40d9dc1
Toluhi Kay <toluhikay@users.noreply.github.com>
2023-05-22 21:45:25 +0100

    revamp images

--- src/components/WhatsAppCall.tsx
+++ src/components/WhatsAppCall.tsx
@@ -3,11 +3,11 @@ import { IoLogoWhatsapp, IoMdCall } from "react-icons/io";
 
 const WhatsAppCall = () => {
   return (
-    <div className="fixed bottom-0 lg:px-[89px] px-[15px] w-full lg:py-12 items-center pb-4 z-[20000] flex justify-between">
-      <div className="bg-secondary text-primary rounded-full p-3 text-3xl ">
+    <div className="fixed bottom-0 lg:px-[25px] px-[15px] w-full lg:py-6 items-center pb-4 z-[20000] flex justify-between">
+      <div className="bg-secondary text-primary rounded-full p-3 text-2xl sm:text-3xl lg:text-5xl ">
         <IoMdCall />
       </div>
-      <div className="bg-secondary text-primary rounded-full p-3 text-3xl ">
+      <div className="bg-secondary text-primary rounded-full p-3 text-2xl sm:text-3xl lg:text-5xl ">
         <IoLogoWhatsapp />
       </div>
     </div>
